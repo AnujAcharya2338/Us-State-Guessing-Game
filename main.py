ff0eab37290b8f8701e47c02fe276d054a7a4c8c
AnujAcharya2338 <anujacharya877@gmail.com>
2025-07-09 23:27:32 -0700

    Data extraction from a csv file while importing csv

---
+++ main.py
@@ -0,0 +1,13 @@
+# with open ("weather_data.csv") as data:
+#      readable_data= data.readlines() 
+#      print(readable_data)
+
+# import csv
+# with open ("weather_data.csv") as data:
+#    w_data = csv.reader(data)
+#    temperature = []
+#    for row in w_data:
+#        if row[1] != "temp":
+#         temperature.append(int(row[1]))
+
+# print(temperature)
