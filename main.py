e4abdb3341a68eafe9b36d718206ad4f515b36b1
AnujAcharya2338 <anujacharya877@gmail.com>
2025-07-27 02:11:13 -0700

    CAlculated count fo squirrel

--- main.py
+++ main.py
@@ -1,51 +1,82 @@
-# with open ("weather_data.csv") as data:
-#      readable_data= data.readlines() 
-#      print(readable_data)
+# # with open ("weather_data.csv") as data:
+# #      readable_data= data.readlines() 
+# #      print(readable_data)
 
-# import csv
-# with open ("weather_data.csv") as data:
-#    w_data = csv.reader(data)
-#    temperature = []
-#    for row in w_data:
-#        if row[1] != "temp":
-#         temperature.append(int(row[1]))
+# # import csv
+# # with open ("weather_data.csv") as data:
+# #    w_data = csv.reader(data)
+# #    temperature = []
+# #    for row in w_data:
+# #        if row[1] != "temp":
+# #         temperature.append(int(row[1]))
 
-# print(temperature)
+# # print(temperature)
+
+# import pandas
+
+# data = pandas.read_csv("weather_data.csv")
+# # print(data)
+# # print(data["temp"])
+
+
+# # data_dict=data.to_dict()
+# # # print(data_dict)
+
+# # temp_list=data["temp"].to_list()
+# # avg = sum(temp_list) / len(temp_list)
+# # # print(avg)
+
+# # data["temp"].max()
+
+# # Get data in a row
+
+# # print(data[data.day == "Monday"])
+
+# # print(data[data.temp == data["temp"].max() ])
+
+# # monday = data[data.day == "Monday"]
+# # monday_temp = monday.temp[0]
+# # faran=monday_temp *9/5 +32
+# # print(faran)
+
+# # Creating a dataframe from scratch
+
+# data_dict = {
+#     "dict":["Amy", "James", "Hanks"] ,
+#     "score":[89,54,97]
+# }
+
+# data = pandas.DataFrame(data_dict)
+# data.to_csv("new_data.csv")
+# print(data)
+
 
 import pandas
+data = pandas.read_csv("squirrel.csv")
 
-data = pandas.read_csv("weather_data.csv")
-# print(data)
-# print(data["temp"])
-
-
-# data_dict=data.to_dict()
-# # print(data_dict)
-
-# temp_list=data["temp"].to_list()
-# avg = sum(temp_list) / len(temp_list)
-# # print(avg)
-
-# data["temp"].max()
-
-# Get data in a row
-
-# print(data[data.day == "Monday"])
-
-# print(data[data.temp == data["temp"].max() ])
-
-# monday = data[data.day == "Monday"]
-# monday_temp = monday.temp[0]
-# faran=monday_temp *9/5 +32
-# print(faran)
-
-# Creating a dataframe from scratch
+grey_squirrel_count= len(data[data["Primary Fur Color"]== "Gray"])
+black_squirrel_count= len(data[data["Primary Fur Color"]== "Black"])
+red_squirrel_count= len(data[data["Primary Fur Color"]== "Cinnamon"])
+# print(grey_squirrel_count)
+# print(black_squirrel_count)
+# print(red_squirrel_count)
 
 data_dict = {
-    "dict":["Amy", "James", "Hanks"] ,
-    "score":[89,54,97]
+    "Fur color": ["Gray", "Cinnamon", "Black"],
+    "Count":[grey_squirrel_count,black_squirrel_count,red_squirrel_count]
 }
 
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
-print(data)
+df = pandas.DataFrame(data_dict)
+df.to_csv("squirrel_count.csv")
+
+
+
+
+
+
+
+
+
+
+
+
