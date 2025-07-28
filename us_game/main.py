616e0ed2b73ed59e950b024a704a09dc7034335a
AnujAcharya2338 <anujacharya877@gmail.com>
2025-07-28 11:05:23 -0700

    Created a us state guessing game and printed the remaining into a csv file!! DING!! DING!!

--- us_game/main.py
+++ us_game/main.py
@@ -11,6 +11,7 @@ while len(guessed_state) < 50:
    ans_state=screen.textinput(title=f"{len(guessed_state)}/50 State guessed ", prompt="Enter the state name:").title()
    df =pd.read_csv('C:/Users/USER/Documents/Python/day25/us_game/50_states.csv')
    states_name=df['state'].to_list()
+   
    if ans_state in states_name:
       t=turtle.Turtle()
       t.hideturtle()
@@ -20,4 +21,11 @@ while len(guessed_state) < 50:
       t.write(ans_state)
       guessed_state.append(ans_state)
    
-turtle.mainloop()
+   if ans_state == "Exit":
+      missing_state=[]
+      for state in states_name:
+         if state not in guessed_state:
+            missing_state.append(state)
+      new_data=pd.DataFrame(missing_state)
+      new_data.to_csv("state_to_learn")
+      break
